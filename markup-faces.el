55dd79251185ddcc0bec6523977437436428a57d
Florian Kaufmann <sensorflo@gmail.com>
2012-02-28 21:08:53 +0100

    updated URL and copyright date

--- markup-faces.el
+++ markup-faces.el
@@ -1,9 +1,9 @@
 ;;; markup-faces.el --- collection of faces for markup language modes
 ;; 
-;; Copyright 2010-2011 Florian Kaufmann <sensorflo@gmail.com>
+;; Copyright 2010-2012 Florian Kaufmann <sensorflo@gmail.com>
 ;;
 ;; Author: Florian Kaufmann <sensorflo@gmail.com>
-;; URL: http://sensorflo-emacs.googlecode.com/svn/trunk/markup-faces.el
+;; URL: https://github.com/sensorflo/markup-faces
 ;; Created: 2010
 ;; Keywords: wp faces
 ;; 
